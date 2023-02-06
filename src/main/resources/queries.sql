46bf76d43ce1026251c20ce21aa9f9acdecfb59e
itui <nebihaamirfeb2@gmail.com>
2023-02-05 22:23:53 -0500

    init commit loan api

---
+++ src/main/resources/queries.sql
@@ -0,0 +1,2 @@
+INSERT INTO loan_application (loanAmount, loanPurpose, loanTerm, applicantName, applicantEmail, applicantIncome, city, state, zip, applicantBirthDate, applicantEmployementStatus)
+VALUES (5000, 'Home improvement', 36, 'John Doe', 'john.doe@example.com', 75000, 'NewYork', 'Buffalo', '20409', '1999-06-15', 'Self-Employeed');
